a230b49262a8ee1cf7573e4ccb8b35367294a58f
martyumlas <martyumlas@gmail.com>
2025-01-21 22:17:43 +0800

    initial current score player 1

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@
 
 const roll = document.querySelector('.btn--roll')
 const dice = document.querySelector('.dice')
+const current_0 = document.getElementById('current--0')
 
 const handleRollDice = () => {
+    const current0 = Number(current_0.textContent)
     dice.classList.remove('hidden')
     const roll = Math.floor(Math.random() * 6) + 1
     dice.src = `/img/dice-${roll}.png`
+
+    current_0.textContent = current0 + roll
 }
 
 roll.addEventListener('click', handleRollDice)
