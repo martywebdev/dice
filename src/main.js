1b58ec10e949e6163a065c62bff62f1eb4e0bf11
martyumlas <martyumlas@gmail.com>
2025-01-21 22:37:01 +0800

    initial scoring

--- src/main.js
+++ src/main.js
@@ -2,18 +2,43 @@
 
 const roll = document.querySelector('.btn--roll')
 const dice = document.querySelector('.dice')
-const current_0 = document.getElementById('current--0')
+const hold = document.querySelector('.btn--hold')
+const player1 = document.getElementById('current--0')
+const player2 = document.getElementById('current--1')
+
+let active = player1
 
 const handleRollDice = () => {
-    const current0 = Number(current_0.textContent)
+    
     dice.classList.remove('hidden')
+
+    const score1 = Number(player1.textContent)
+    const score2 = Number(player2.textContent)
+
+
+
     const roll = Math.floor(Math.random() * 6) + 1
+
     dice.src = `/img/dice-${roll}.png`
 
-    current_0.textContent = current0 + roll
+    player1.textContent = score1 + roll
+}
+
+const handleHold = () => {
+    active = player2
+
+    if (active == player2) {
+        player1.classList.remove('player--active')
+        player2.classList.add('player--active')
+    } else {
+        player2.classList.remove('player--active')
+        player1.classList.add('player--active')
+    }
+    console.log(active)
 }
 
 roll.addEventListener('click', handleRollDice)
+hold.addEventListener('click', handleHold)
 
 const initialize = () => {
     dice.classList.add('hidden')
